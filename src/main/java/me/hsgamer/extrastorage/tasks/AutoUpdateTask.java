6ab0b6498942c53db54158e3683cbe7441fed517
hsgamer <huynhqtienvtag@gmail.com>
2025-07-23 22:31:18 +0700

    remove ESTask

--- src/main/java/me/hsgamer/extrastorage/tasks/AutoUpdateTask.java
+++ src/main/java/me/hsgamer/extrastorage/tasks/AutoUpdateTask.java
@@ -2,17 +2,24 @@ package me.hsgamer.extrastorage.tasks;
 
 import me.hsgamer.extrastorage.ExtraStorage;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public final class AutoUpdateTask
-        extends ESTask {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class AutoUpdateTask extends BukkitRunnable {
+    private final ExtraStorage instance;
 
     public AutoUpdateTask(ExtraStorage instance, int time) {
-        super(instance, time, true, true);
+        this.instance = instance;
+        long timeInTicks = time * 20L;
+        this.runTaskTimerAsynchronously(instance, timeInTicks, timeInTicks);
     }
 
     @Override
-    public void onFinish() {
-        Bukkit.getServer().getOnlinePlayers().forEach(player -> instance.getUserManager().getUser(player).save());
+    public void run() {
+        List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+        players.forEach(player -> instance.getUserManager().getUser(player).save());
     }
-
 }
