ce4a0ae37c26570a9ce38f05c9324156d3e341bb
hsgamer <huynhqtienvtag@gmail.com>
2026-03-27 15:49:40 +0700

    use compareItemByQuantity if the main one is 0

--- src/main/java/me/hsgamer/extrastorage/gui/util/SortUtil.java
+++ src/main/java/me/hsgamer/extrastorage/gui/util/SortUtil.java
@@ -13,9 +13,7 @@ public final class SortUtil {
     public static int compareItemByMaterial(Item obj1, Item obj2, boolean orderSort) {
         int compare = orderSort ? obj1.getKey().compareTo(obj2.getKey()) : obj2.getKey().compareTo(obj1.getKey());
         if (compare == 0) {
-            compare = orderSort
-                    ? Integer.compare((int) Math.min(obj2.getQuantity(), Integer.MAX_VALUE), (int) Math.min(obj1.getQuantity(), Integer.MAX_VALUE))
-                    : Integer.compare((int) Math.min(obj1.getQuantity(), Integer.MAX_VALUE), (int) Math.min(obj2.getQuantity(), Integer.MAX_VALUE));
+            compare = compareItemByQuantity(obj1, obj2, orderSort);
         }
         return compare;
     }
@@ -26,9 +24,7 @@ public final class SortUtil {
         String name2 = setting.getNameFormatted(obj2.getKey(), false);
         int compare = orderSort ? name1.compareTo(name2) : name2.compareTo(name1);
         if (compare == 0) {
-            compare = orderSort
-                    ? Integer.compare((int) Math.min(obj2.getQuantity(), Integer.MAX_VALUE), (int) Math.min(obj1.getQuantity(), Integer.MAX_VALUE))
-                    : Integer.compare((int) Math.min(obj1.getQuantity(), Integer.MAX_VALUE), (int) Math.min(obj2.getQuantity(), Integer.MAX_VALUE));
+            compare = compareItemByQuantity(obj1, obj2, orderSort);
         }
         return compare;
     }
